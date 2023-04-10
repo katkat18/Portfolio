9ba10475bdceb62c2962d641bb67bd6803ac12cf
katrinajamir <katrinajamir@gmail.com>
2023-04-09 19:22:20 -0700

    first push to github

---
+++ js/main.js
@@ -0,0 +1,78 @@
+let isClicked = false;
+let isWhite = true;
+
+$('#menu-btn').click(function (e) {
+    e.preventDefault();
+    if(isClicked){
+        $('#menu-img').attr({
+            'src':'images/menu_pink2.svg',
+            'alt': 'menu'
+        });
+        $('.sidebar').hide();
+        isClicked = false;
+    }else{
+        $('#menu-img').attr({
+            'src':'images/x.svg',
+            'alt': 'close menu'
+        });
+        $('.sidebar').show();
+        isClicked = true;
+    }
+
+    //isClicked ? false : true;
+    //$('.sidebar').slideToggle('fast');
+
+});
+
+//close menu whenever it is clicked 
+$('.menu-item').click(function() {
+    if($(window).width() <= 1024){
+        $('.sidebar').hide();
+        $('#menu-img').attr({
+            'src':'images/menu_pink2.svg',
+            'alt': 'menu'
+        });
+        isClicked = false;
+    }
+});
+
+$(window).resize(function() {
+    console.log(`window size: ${$(window).width()}`);
+    if($(window).width() < 1009 && !isClicked){
+        $('.sidebar').hide();
+        $('#menu-img').attr({
+            'src':'images/menu_pink2.svg',
+            'alt': 'menu'
+        });  
+    }else if($(window).width() < 1009 && isClicked) {
+        $('.sidebar').show(); 
+        $('#menu-img').attr({
+            'src':'images/x.svg',
+            'alt': 'close menu'
+        });
+    }else{
+        $('.sidebar').show();
+        $('#menu-img').attr({
+            'src':'images/menu_pink2.svg',
+            'alt': 'menu'
+        });
+    }
+});
+
+
+//menu hover
+$('.clickables').mouseenter(function() {
+    console.log(`hovering on: ${$(this).data('hover')}`);
+    $(this).attr({
+        'src': `images/${$(this).data('hover')}_hover.svg`,
+        'alt': `${$(this).data('hover')}`
+    });
+});
+
+$('.clickables').mouseleave(function() {
+    console.log(`hovering on: ${$(this).data('hover')}`);
+    $(this).attr({
+        'src': `images/${$(this).data('hover')}.svg`,
+        'alt': `${$(this).data('hoever')}`
+    });
+});
